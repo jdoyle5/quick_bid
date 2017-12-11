a87a05aa3c180daa9f7fe2603fe29ad1227d827e
Wilson <wchun108@gmail.com>
2017-12-10 21:54:36 -0800

    Complete O Auth login

---
+++ QuickBid/backend/config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '1550753065013217',
+  clientSecret: '7afbe62a182ae2c90c0bac6ddbbe6512',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '626455213616-4s2vqggfa6h4hiq497fubaasb67bkg4g.apps.googleusercontent.com',
+  clientSecret: 'MHnYkM8xgMl6v_MGjgwtarUH',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
