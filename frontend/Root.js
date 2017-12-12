0abf2b419db5fc2121d3a67ea6f15a92a65e6270
Wilson <wchun108@gmail.com>
2017-12-11 22:14:00 -0800

    Add navigator and second screen test

--- frontend/Root.js
+++ frontend/Root.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import App from './components/App';
+import { StackNavigator } from "react-navigation";
+import SecondScreen from "./components/Test/test";
 
 
 import configureStore from './store/store';
@@ -9,14 +11,22 @@ const store = configureStore({});
 
 console.log(store.getState());
 
-class Root extends Component {
-  render () {
-    return (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  }
-}
+
+const reactNavigationSample = props => {
+  return (
+    <Provider store={store}>
+      <App navigation={props.navigation} />
+    </Provider>
+  );
+};
+
+reactNavigationSample.navigationOptions = {
+  title: "Login"
+};
+
+const Root = StackNavigator({
+  Home: { screen: reactNavigationSample },
+  SecondScreen: { screen: SecondScreen, title: "Second Screen" }
+});
 
 export default Root;
--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,15 +3,24 @@ import {
   Text,
   View,
   StyleSheet,
+  Button
 } from 'react-native';
-
+import { StackNavigator } from "react-navigation";
 import AuthContainer from './OAuth/auth_container';
 
-const App = () => (
-  <View style={styles.container}>
-    < AuthContainer />
-  </View>
-);
+const App = (props) => {
+  const { navigate } = props.navigation;
+
+  return (
+    <View style={styles.container}>
+      < AuthContainer />
+      <Button
+           onPress={() => navigate('SecondScreen')}
+           title="Go to Second Screen"
+         />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
