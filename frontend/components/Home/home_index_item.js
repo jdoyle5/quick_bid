28bce7a9a4cc846a82e97df197f5dbfd1c12ec04
Joey Doyle <j.doyle345@gmail.com>
2017-12-14 14:50:41 -0800

    Add some styling to the home page

--- frontend/components/Home/home_index_item.js
+++ frontend/components/Home/home_index_item.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, ScrollView, Image } from 'react-native';
+import { Text, View, ScrollView, Image, StyleSheet } from 'react-native';
 import { receiveItems } from '../../actions/item_actions';
 
 export default class HomeIndexItem extends Component {
@@ -8,9 +8,48 @@ export default class HomeIndexItem extends Component {
     let image = {uri: item.img_url};
 
     return (
-        <View>
-          <Image source={image} style={{width: '100%', height: 200}}/>
+        <View style={styles.content}>
+          <Text style={styles.header}>{item.title}</Text>
+        <Image source={image} style={{width: '80%', height: 300, marginBottom: 40}}/>
         </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20,
+    backgroundColor: '#00FFFF'
+  },
+  avatar: {
+    margin: 20,
+  },
+  avatarImage: {
+    borderRadius: 50,
+    height: 100,
+    width: 100,
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  text: {
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 5,
+  },
+  buttons: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    margin: 20,
+    marginBottom: 30,
+  },
+});
