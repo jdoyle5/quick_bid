4c45a9d17aee12194df01822969a4dc921378953
Wilson <wchun108@gmail.com>
2017-12-14 10:44:01 -0800

    Add navigator to app

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,22 +5,44 @@ import {
   StyleSheet,
   Button
 } from 'react-native';
-import { StackNavigator } from "react-navigation";
+import { StackNavigator, TabNavigator } from "react-navigation";
 import AuthContainer from './OAuth/auth_container';
 import HomeContainer from './Home/home_container';
 import AuctionWindowContainer from './Auction/auction_window_container';
 
-const App = (props) => {
-  // const { navigate } = props.navigation;
-
+// const App = (props) => {
+//   // const { navigate } = props.navigation;
+//
+//   return (
+//     <View style={styles.container}>
+//       < AuctionWindowContainer />
+//       < AuthContainer />
+//       < HomeContainer />
+//     </View>
+//   );
+// };
+const Home = props => {
   return (
-    <View style={styles.container}>
-      < AuctionWindowContainer />
-      < AuthContainer />
-      < HomeContainer />
-    </View>
+      <HomeContainer navigation={props.navigation} />
   );
 };
+const Authentication = props => {
+  return (
+      <AuthContainer navigation={props.navigation} />
+  );
+};
+const AuctionWindow = props => {
+  return (
+      <AuctionWindowContainer navigation={props.navigation} />
+  );
+};
+
+const App = TabNavigator({
+  Home: { screen: Home },
+  Login: { screen: Authentication, title: "Login" },
+  AuctionWindow: {screen: AuctionWindow, title: "Live"}
+});
+
 
 const styles = StyleSheet.create({
   container: {
